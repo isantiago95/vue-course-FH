9c0785a5592dcdfe31d21805b4a46676b324953f
Israel Santiago <israel.santiago4359@gmail.com>
2023-09-06 15:36:50 -0700

    245. diseno del state

--- 08-composition-api/src/store/index.js
+++ 08-composition-api/src/store/index.js
@@ -1,7 +1,14 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    todos: [
+      { id: '1', text: 'Recolectar piedras infinito', completed: false },
+      { id: '2', text: 'Piedra del alma', completed: true },
+      { id: '3', text: 'Piedra del poder', completed: true },
+      { id: '4', text: 'Piedra del realidad', completed: false },
+      { id: '5', text: 'Conseguir nuevos secuaces competentes', completed: false },
+    ]
   },
   getters: {
   },
@@ -11,4 +18,4 @@ export default createStore({
   },
   modules: {
   }
-})
+});
---
+++ 08-composition-api/src/views/TodoVuex.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Thanos To-Do List</h1>
+
+    <h4>Tasks: {{ $store.state.todos.length }}</h4>
+</template>
+
+<script>
+
+export default {
+    setup() {
+
+    }
+}
+
+</script>
+
+<style scoped></style>
