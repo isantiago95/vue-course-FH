87bd319edcc5ba2e4f8d53598b1fd96c0df19d8d
Israel Santiago <israel.santiago4359@gmail.com>
2023-09-06 14:13:21 -0700

    238. composable usePokemon

---
+++ 08-composition-api/src/hooks/usePokemon.js
@@ -0,0 +1,37 @@
+import { ref } from 'vue';
+import axios from 'axios';
+
+const usePokemon = (pokemonId = '1') => {
+
+    const pokemon = ref();
+    const isLoading = ref(false);
+    const errorMessage = ref();
+
+    const searchPokemon = async () => {
+        isLoading.value = true;
+        pokemon.value = null;
+
+        try {
+            const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+
+            pokemon.value = data;
+            errorMessage.value = null;
+
+            console.log(data);
+        } catch (error) {
+            errorMessage.value = `Error loading pokemon with id: ${pokemonId}`;
+        }
+
+        isLoading.value = false;
+    };
+
+    searchPokemon();
+
+    return {
+        pokemon,
+        isLoading,
+        errorMessage,
+    };
+};
+
+export default usePokemon;
--- 08-composition-api/src/views/Pokemon.vue
+++ 08-composition-api/src/views/Pokemon.vue
@@ -1,21 +1,42 @@
 <template>
-    <h1>Pokemon View</h1>
+    <h1 v-if="!pokemon && !errorMessage">Cargando...</h1>
+    <h1 v-else-if="errorMessage">{{ errorMessage }}</h1>
+
+    <template v-else>
+        <h3>{{ pokemon.name }}</h3>
+        <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+        <br>
+        <router-link :to="{ name: 'pokemon-search' }">Regresar</router-link>
+    </template>
 </template>
 
 <script>
 import { useRoute } from 'vue-router';
+import usePokemon from '../hooks/usePokemon';
 
 export default {
 
     setup() {
-
         const route = useRoute();
-
         console.log(route.params);
+
+        const { pokemon, isLoading, errorMessage } = usePokemon(route.params.id);
+
+        return {
+            pokemon, isLoading, errorMessage
+        };
     }
 
 }
 
 </script>
 
-<style scoped></style>
+<style scoped>
+h3 {
+    font-size: 2em;
+}
+
+img {
+    width: 10em;
+}
+</style>
