680bbd4647bed6f6e3be3752fbbafb6520063d0d
Israel Santiago <israel.santiago4359@gmail.com>
2023-09-10 18:15:08 -0700

    276. interceptores

--- 07-journal-vuex/src/api/journalApi.js
+++ 07-journal-vuex/src/api/journalApi.js
@@ -1,13 +1,20 @@
-
-import axios from 'axios';
-
+import axios from "axios";
 
 const journalApi = axios.create({
-    baseURL: 'https://vue-course-fh-default-rtdb.firebaseio.com'
+    baseURL: "https://vue-course-fh-default-rtdb.firebaseio.com",
 });
 
-// console.log( process.env.NODE_ENV ) // TEST durante testing, 
+journalApi.interceptors.request.use((config) => {
+    const auth = JSON.parse(localStorage.getItem('idToken'));
 
-export default journalApi
+    config.params = {
+        ...config.params,
+        auth,
+    };
 
+    return config;
+});
 
+// console.log( process.env.NODE_ENV ) // TEST durante testing,
+
+export default journalApi;
